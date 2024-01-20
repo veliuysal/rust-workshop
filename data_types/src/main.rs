e55fe8cb1f9076399a01f44dad478ff7f6e988ca
Veli Uysal <veliuysaldev@gmail.com>
2024-01-20 22:43:22 +0300

    scalars(integers)

---
+++ data_types/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    // --- Integer Types ---
+
+    // u -> unsigned, i -> signed
+    // 8 bit -> i8, u8
+    // 16 bit -> i16, u16
+    // 32 bit -> i32, u32
+    // 64 bit -> i64, u64
+    //128 bit -> i128, u128
+    //arch -> isize, usize (depends on os architecture)
+    // default -> i32
+
+    // - Examples -
+    //let i8_var : i8 = 12;
+    //let u8_var : u8  = 32;
+
+    //let i16_var : i16 = 345;
+    //let u16_var :u16 = 590;
+
+    //let i32_var = 34;
+    //let u32_var:u32 = 45;
+
+    //let i64_var : i64 = 4534;
+    //let u64_var : u64 = 234234;
+
+    //let i128_var : i128 = 454534;
+    //let u128_var : u128 = 5454;
+
+    // --- Integer Literals ---
+    // Decimal -> 6_34
+    // Hex -> 0x4e2a
+    // Octal -> 0o562
+    // Binary -> 0b110101_11
+    // Byte(u8 only) -> b'V'
+
+    // - Examples
+
+
+
+}
+
+

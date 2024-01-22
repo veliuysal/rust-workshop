275b1ca67418e7bcac030dd6d137ce408825a79c
Veli Uysal <veliuysaldev@gmail.com>
2024-01-22 20:01:48 +0300

    scalars(integers)

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    // --- Integer Types ---
 
+    //################ SCALAR TYPES ################
+    // --- Integer Types ---
     // u -> unsigned, i -> signed
-    // 8 bit -> i8, u8
-    // 16 bit -> i16, u16
-    // 32 bit -> i32, u32
-    // 64 bit -> i64, u64
-    //128 bit -> i128, u128
+    // 8 bits -> i8, u8
+    // 16 bits -> i16, u16
+    // 32 bits -> i32, u32
+    // 64 bits -> i64, u64
+    //128 bits -> i128, u128
     //arch -> isize, usize (depends on os architecture)
     // default -> i32
 
@@ -33,7 +34,59 @@ fn main() {
     // Binary -> 0b110101_11
     // Byte(u8 only) -> b'V'
 
-    // - Examples
+    // -------------------------------------------------------
+    // --- Floating-Points Types ---
+    //32 bits -> f32
+    //64 bits -> f64
+
+    // - Examples -
+    //let x = 2.0; // f64
+    //let y: f32 = 3.0; // f32
+
+    // -------------------------------------------------------
+    // --- Boolean Type ---
+    //1 byte -> true or false
+
+    // - Examples -
+    //let t = true;
+    //let f: bool = false; // with explicit type annotation
+
+    // -------------------------------------------------------
+    // --- Character Type ---
+    //4 bytes -> Unicode Scalar Value
+
+    // - Examples -
+    //let c = 'z';
+    //let z: char = 'ℤ'; // with explicit type annotation
+    //let heart_eyed_cat = '😻';
+
+    //################ COMPOUND TYPES ################
+    // --- The Tuple Type ---
+    //Fixed length
+    //let tuple1: (i32, f64, u8) = (500, 6.4, 1);
+    //let tuple2: (f32, u64, i16) = (59.6, 454, 34);
+
+    //let (x1, y1, z1) = tuple1;
+    //let first_var_tuple2 = tuple2.0;
+    //let second_var_tuple1 = tuple1.1;
+    //let one = tuple1.2;
+
+    // --- The Array Type ---
+    //Fixed length, same type
+    //let a = [1, 2, 3, 4, 5];
+    //let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+    //let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    //let a = [3; 5];
+    //let a = [3, 3, 3, 3, 3];
+
+    // - Usage -
+    //let first = a[0];
+    //let second = a[1];
+
+
+
+
 
 
 
